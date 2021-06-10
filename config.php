7dc131bdc8d64357ec0c55d138fdd729193365c0
Kartik12321 <81910181+Kartik12321@users.noreply.github.com>
2021-06-10 09:04:23 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$conn = mysqli_connect('localhost','id16992224_customer','$9Ni6dkYe(9xRm*)','id16992224_customerdetails');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
